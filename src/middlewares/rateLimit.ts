1387bd012bed28d78e8bf0db508c708241a5d8fc
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2025-03-18 12:52:40 -0300

    fix: :bug:

--- src/middlewares/rateLimit.ts
+++ src/middlewares/rateLimit.ts
@@ -10,8 +10,19 @@ export const postLimiter = rateLimit({
 });
 
 export const getLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 minutos
-  max: 500, // 100 solicitudes por token
+  windowMs: 60 * 1000, // 10 minutos
+  max: 100, // 100 solicitudes por token
   keyGenerator: (req: Request) => req.token || 'anonymous',
   message: { message: 'Demasiadas solicitudes, intentá de nuevo en 10 minutos' },
 });
+
+const limiter = rateLimit({
+	windowMs: 60 * 1000, // 10 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	keyGenerator: (req) => req.ip || "anonymous",
+  handler: (req, res) => {
+    res.status(429).json({ message: 'Demasiadas solicitudes, intentá de nuevo en 10 minutos' });
+  },
+})
